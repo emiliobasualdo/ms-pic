defc6a13bf4a922ea29199515151233205ffe2cf
agustinamartinez <agus10.cba@gmail.com>
2020-08-21 06:26:37 -0300

    clientes/nuevos page finished

--- src/pages/Customers/New/New.js
+++ src/pages/Customers/New/New.js
@@ -1,16 +1,152 @@
 import React from 'react';
-import TitleCard from '../../../components/TitleCard/TitleCard';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
+//Mocks
+import BarGraphsData from '../../../mocks/customers/new/barGraphs.json';
+import GenresData from '../../../mocks/customers/new/genre.json';
+import AgeRangeData from '../../../mocks/customers/new/ageRange.json';
+import GeographicZonesData from '../../../mocks/customers/new/geographicZones.json';
+import MaleGenreData from '../../../mocks/customers/new/maleGenre.json';
+import FemaleGenreData from '../../../mocks/customers/new/femaleGenre.json';
+//Components
+import{
+    TitleCard,
+    BarGraphCard,
+    TwoCircularProgressCard,
+    Button,
+    CircularProgressContainer,
+    SquareProgressCircular,
+    LargeCircularProgressCard
+} from '../../../components';
+//Styles
+import {
+    CustomersNewContent,
+    BarGraphCardsContainer,
+    CircularProgressCardsContainer
+} from './New.styles';
 
 const New = () => {
     return(
-        <TitleCard
-            icon={<PersonAddIcon fontSize="large"/>}
-            title="Detalle de Clientes Nuevos"
-            diary="Diaria"
-            weekly="Semanal"
-            monthly="Mensual"
-        />
+        <CustomersNewContent>
+            <TitleCard
+                icon={<PersonAddIcon fontSize="large"/>}
+                title="Detalle de Clientes Nuevos"
+                diary="Diaria"
+                weekly="Semanal"
+                monthly="Mensual"
+            />
+            <BarGraphCardsContainer>
+                {
+                    BarGraphsData.map((card, index) => {
+                        return(
+                            <BarGraphCard
+                                key={index}
+                                barWidth="100"
+                                barHeight="50"
+                                number={card.number}
+                                description={card.description}
+                            />
+                        )
+                    })
+                }
+            </BarGraphCardsContainer>
+            <CircularProgressCardsContainer>
+                <TwoCircularProgressCard>
+                    {
+                        GenresData.map((genre, index) => {
+                            return(
+                                <CircularProgressContainer
+                                    key={index}
+                                    percentage={genre.percentage}
+                                    progressDescription={genre.description}
+                                    width="35%"
+                                    textColor="blue"
+                                    pathColor="blue"
+                                />
+                            )
+                        })
+                    }
+                    <Button buttonName="Más Detalles"/>
+                </TwoCircularProgressCard>
+                <SquareProgressCircular
+                    width="28%"
+                    title="Zonas Geográficas"
+                    buttonName="Más Detalles"
+                >
+                    {
+                        GeographicZonesData.map((circle, index) => {
+                            return(
+                                <CircularProgressContainer
+                                    key={index}
+                                    percentage={circle.percentage}
+                                    progressDescription={circle.description}
+                                    width="30%"
+                                    height="60%"
+                                    textColor="rgb(90, 191, 141)"
+                                    pathColor="rgb(90, 191, 141)"
+                                />
+                            )
+                        })
+                    }
+                </SquareProgressCircular>
+                <SquareProgressCircular
+                    width="28%"
+                    title="Rango de Edad"
+                    buttonName="Más Detalles"
+                >
+                    {
+                        AgeRangeData.map((circle, index) => {
+                            return(
+                                <CircularProgressContainer
+                                    key={index}
+                                    percentage={circle.percentage}
+                                    progressDescription={circle.description}
+                                    width="30%"
+                                    height="60%"
+                                    textColor="rgb(90, 191, 141)"
+                                    pathColor="rgb(90, 191, 141)"
+                                />
+                            )
+                        })
+                    }
+                </SquareProgressCircular>
+            </CircularProgressCardsContainer>
+            <LargeCircularProgressCard title="Género Femenino">
+                {
+                    FemaleGenreData.map((circle, index) => {
+                        return(
+                            <CircularProgressContainer
+                                key={index}
+                                percentage={circle.percentage}
+                                progressDescription={circle.description}
+                                width="8%"
+                                height="10%"
+                                textColor="salmon"
+                                pathColor="salmon"
+                                margin="1rem 3rem"
+                            />
+                        )
+                    })
+                }
+            </LargeCircularProgressCard>
+            <LargeCircularProgressCard title="Género Masculino">
+                {
+                    MaleGenreData.map((circle, index) => {
+                        return(
+                            <CircularProgressContainer
+                                key={index}
+                                percentage={circle.percentage}
+                                progressDescription={circle.description}
+                                width="8%"
+                                height="10%"
+                                textColor="blue"
+                                pathColor="blue"
+                                margin="1rem 3rem"
+                            />
+                        )
+                    })
+                }
+            </LargeCircularProgressCard>
+        </CustomersNewContent>
     );
 }
 
---
+++ src/pages/Customers/New/New.styles.js
@@ -0,0 +1,26 @@
+import styled, {keyframes}from 'styled-components';
+
+export const fadeIn = keyframes`
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 100%;
+    }
+`;
+
+export const CustomersNewContent = styled.div`
+    width: auto;
+    animation: ${fadeIn} .5s .2s ease-in backwards;
+`;
+
+export const BarGraphCardsContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const CircularProgressCardsContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 2rem 0;
+`;
